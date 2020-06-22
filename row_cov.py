d66ab0d8aca02690476e0ad994cc023c49a8281c
Karl Norrena <karl.norrena@gmail.com>
2020-06-22 14:25:20 -0600

    fixed missing file in row_cov.py

--- row_cov.py
+++ row_cov.py
@@ -16,7 +16,7 @@ class tickers:
         does not integrate the covid data"""
         
         import pandas as pd
-        df = pd.read_csv('    ')
+        df = pd.read_csv('tkr_data.csv')
         #print(list(df))
         #['high', 'low', 'open', 'close', 'volume', 'adjustedclose', 'ticker', 'DateStamp', 
         # 'Industry', 'Sector', 'Country', 'Commodity']
